429b1efb23b55bb385fa7f854ee495a27035f639
galinadychko <galiadychko@outlook.com>
2019-05-02 18:50:20 +0300

    docs: Add TO DO

--- assignment1/utils/general_utils/classifiers_utils.py
+++ assignment1/utils/general_utils/classifiers_utils.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 
+#####################################################################
+# TODO:                                                             #
+# Write documentation                                               #
+#####################################################################
+
 def mode(vector):
     result = np.argmax(np.bincount(np.array(vector, "int32")))
     return result
